ddcf876fbeecefe8be0d3b05392a3a10afdd91d3
sokol <devsokol555@izigroup.nyc>
2019-03-10 11:06:16 +0200

    edit bootstrap

--- src/components/Point.vue
+++ src/components/Point.vue
@@ -8,6 +8,17 @@
       <input type="text" placeholder="password" v-model="password">
       <button @click="signin">Sign in</button>
 
+    <div class="dropdown">
+      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+      </button>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+          <button class="dropdown-item" type="button">Action</button>
+          <button class="dropdown-item" type="button">Another action</button>
+          <button class="dropdown-item" type="button">Something else here</button>
+      </div>
+    </div>
+
     </div>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
 
 Vue.config.productionTip = false
 
