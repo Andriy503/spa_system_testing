96e4b401b76281b62dbe7217b5b0f61238987f64
sokol <devsokol555@izigroup.nyc>
2019-05-25 18:26:17 +0300

    show question & answers testing

--- src/components/testing.vue
+++ src/components/testing.vue
@@ -4,7 +4,97 @@
     <preLoader v-if="preLoader"></preLoader>
 
     <div class="content-testing" v-else>
-      <h1>Cooll</h1>
+      <div class="test-page">
+        <h1 class="test-title text-center">{{ ticket.title }}</h1>
+        <div class="test-container">
+
+          <!-- navigator -->
+          <div class="test-navigation">
+            <button
+              class="navi-btn"
+              :class="{'active': paginationNumber === i}"
+              v-for="(i, index) in questions.length"
+              :key="index"
+              @click="changeNumberPagination(i)"
+            >
+            {{ i }}
+            </button>
+          </div>
+
+          <!-- content testing -->
+          <div class="test-content">
+
+            <!-- question -->
+            <div class="test-question">
+              {{ currentQuestion.title }}
+            </div>
+
+            <!-- block answers -->
+            <div class="answers-block">
+
+              <div class="answer-element" v-for="(answer, index) in currentQuestion.answers" :key="index">
+
+                <!-- One Assoc -->
+                <div v-if="currentQuestion.id_type === 1">
+                  <input type="radio" id="answer1" value="1" name="answerOneAssoc">
+                  <label>
+                    {{ answer.title }}
+                  </label>
+                </div>
+
+                <!-- Many Assoc -->
+                <div v-if="currentQuestion.id_type === 2">
+                  <input type="checkbox" id="answer1" value="1" name="answerManyAssoc">
+                  <label>
+                    {{ answer.title }}
+                  </label>
+                </div>
+
+                <!-- Assoc -->
+                <div v-if="currentQuestion.id_type === 3" class="face-wrapper">
+                  <!-- bundles questions -->
+                  <div class="block-answer-1">
+                    <ul id="ul-block-one">
+                      <li value="10" v-for="(i, index) in currentQuestion.move_bundles.questions" :key="index">
+                        {{ i.title }}
+                      </li>
+                    </ul>
+                  </div>
+
+                  <!-- bundles answers -->
+                  <div class="block-answer-2">
+                    <ul id="ul-block-two">
+                      <li value="10" v-for="(i, index) in currentQuestion.move_bundles.answers" :key="index">
+                        {{ i.title }}
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+
+                <!-- Word -->
+                <div v-if="currentQuestion.id_type === 4">
+                  <textarea type="text"
+                  class="form-control"
+                  placeholder="Введіть відповідь"
+                  rows="4"
+                ></textarea>
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+
+          <!-- btn next and back -->
+          <div class="test-control">
+            <button class="test-control-btn">
+              Далі
+            </button>
+          </div>
+
+        </div>
+      </div>
     </div>
 
   </div>
@@ -25,7 +115,11 @@ export default {
   data () {
     return {
       entrant: {},
-      preLoader: false
+      preLoader: false,
+      questions: [],
+      currentQuestion: {},
+      ticket: {},
+      paginationNumber: 1
     }
   },
   methods: {
@@ -35,7 +129,12 @@ export default {
       api.getDataAndCheckRootUser({id: this.entrant.id})
         .then(res => {
           if (res.data.success) {
-            console.log(res.data.data)
+            this.questions = res.data.data.questions
+            this.ticket = res.data.data.ticket
+
+            console.log(this.questions)
+
+            this.currentQuestion = this.questions[0]
           } else {
             toastr.error(res.data.message)
             this.$router.push('/access-entrants')
@@ -47,6 +146,12 @@ export default {
         .catch(resErr => {
           console.log('Помилка в блоці catch: ', resErr)
         })
+    },
+    changeNumberPagination (number) {
+      this.paginationNumber = number
+
+      let index = number - 1
+      this.currentQuestion = this.questions[index]
     }
   },
   created () {
